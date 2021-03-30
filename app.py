d6ee7160d55c4f36c465c522b53772a494dbc004
sager97267 <69594595+sager97267@users.noreply.github.com>
2021-03-30 10:05:12 -0700

    base.html page set up.  start of survay set

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import Survey 
+from surveys import satisfaction_survey as survey 
 
 
 app=Flask(__name__)
 app.config['SECRET_KEY']="jhfldasufadfhaoui"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug=DebugToolbarExtension(app)
 
 @app.route('/home')
+# creating home page route for survey
 def home_page():
     
-    return render_template('index.html')
+    return render_template('index.html',survey=survey)
+
+@app.route('/start')
+def question():
+    return render_template('question.html')
+
 
 
 
