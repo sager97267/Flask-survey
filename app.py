16c242232d25b68a9ba17cbdcec65548b8b18984
sager97267 <69594595+sager97267@users.noreply.github.com>
2021-03-29 20:25:02 -0700

    Debug Toolbar added

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask,request
+from flask_debugtoolbar import DebugToolbarExtension
 
 app=Flask(__name__)
+app.config['SECRET_KEY']="jhfldasufadfhaoui"
+
+debug=DebugToolbarExtension(app)
 
 @app.route('/home')
 def home_page():
