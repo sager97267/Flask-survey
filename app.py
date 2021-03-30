54c7d91de886901720d85e13c344e8b75b8a0030
sager97267 <69594595+sager97267@users.noreply.github.com>
2021-03-30 03:56:48 -0700

    base.html added

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import Survey 
+
 
 app=Flask(__name__)
 app.config['SECRET_KEY']="jhfldasufadfhaoui"
@@ -8,4 +10,13 @@ debug=DebugToolbarExtension(app)
 
 @app.route('/home')
 def home_page():
-    return "hello!"
+    
+    return render_template('index.html')
+
+
+
+
+
+
+
+responses=[]
