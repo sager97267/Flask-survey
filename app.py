d7a0e990ee413d315f4c7100cc00a1bc1d121361
sager97267 <69594595+sager97267@users.noreply.github.com>
2021-04-01 16:12:03 -0700

    survay flows from start to finish, ?'s appear

--- app.py
+++ app.py
@@ -17,19 +17,31 @@ def home_page():
 
 @app.route('/start')
 def question():
+    awnser1=request.form['awnser']
+    responses.append(awnser1)
     return render_template('question1.html',survey=survey)
 
 @app.route('/question2')
 def question2():
+    awnser2=request.form['awnser']
+    responses.append(awnser2)
     return render_template('question2.html',survey=survey)
 
-app.route('/question3')
+@app.route('/question3')
 def question3():
+    awnser3=request.form['awnser']
+    responses.append(awnser3)
     return render_template('question3.html',survey=survey)
 
-app.route('/question4')
+@app.route('/question4')
 def question4():
+    awnser4=request.form['awnser']
+    responses.append(awnser4)
     return render_template('question4.html',survey=survey)
+
+@app.route('/compleat')
+def survay_compleated():
+    return render_template('compleat.html')
 # @app.route('/question2')
 # def question2():
 #     return render_template('/question3.html',survey=survey)
