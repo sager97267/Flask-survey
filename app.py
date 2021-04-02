302df4dda14b33054ff0bd051c921e53bd282a05
sager97267 <69594595+sager97267@users.noreply.github.com>
2021-04-01 19:37:19 -0700

    Thusday changes

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug=DebugToolbarExtension(app)
 
+responses=[]
 @app.route('/')
 # creating home page route for survey
 def home_page():
@@ -16,44 +17,53 @@ def home_page():
     return render_template('index.html',survey=survey)
 
 @app.route('/start')
-def question():
-    awnser1=request.form['awnser']
-    responses.append(awnser1)
-    return render_template('question1.html',survey=survey)
+def start():
+    return redirect('/question/0')
+   
+    # return render_template('question.html',survey=survey)
+
+@app.route('/question/<int:num>')
+def question(num):
+    # num=request.args.get('num')
+    return render_template('question.html',survey=survey,num=num)
 
 @app.route('/question2')
 def question2():
-    awnser2=request.form['awnser']
-    responses.append(awnser2)
+    # awnser2=request.form['awnser']
+    # responses.append(awnser2)
     return render_template('question2.html',survey=survey)
 
 @app.route('/question3')
 def question3():
-    awnser3=request.form['awnser']
-    responses.append(awnser3)
+    # awnser3=request.form['awnser']
+    # responses.append(awnser3)
     return render_template('question3.html',survey=survey)
 
 @app.route('/question4')
 def question4():
-    awnser4=request.form['awnser']
-    responses.append(awnser4)
+    # awnser4=request.form['awnser']
+    # responses.append(awnser4)
     return render_template('question4.html',survey=survey)
 
 @app.route('/compleat')
 def survay_compleated():
-    return render_template('compleat.html')
+    return render_template('compleat.html',responses=responses)
 # @app.route('/question2')
 # def question2():
 #     return render_template('/question3.html',survey=survey)
     
 
+@app.route('/awnser', methods=["POST"])
+def collect_awnsers():
+    awnser=request.form['awnser']
+    responses.append(awnser)
+
+    if len(responses)==4:
+        return redirect('/compleat')
+
+    else:
+        return redirect(f'question/{len(responses)}')
 
 
 
-# ,'/question2.html','/question3.html','question4.html'
 
-
-
-
-
-responses=[]
